9f8e737002caffa8bfeb9b6ec032cb41ebe535f2
josmas <josmasflores@gmail.com>
2023-10-15 16:53:16 +0100

    fixing default indentation

--- src/promptingaround/app.py
+++ src/promptingaround/app.py
@@ -1,16 +1,16 @@
 from icecream import ic
 
 class Application():
-  def run(self) -> int:
-    ic("Placeholder for future setup")
-    return 0
+    def run(self) -> int:
+      ic("Placeholder for future setup")
+      return 0
 
 
 def main() -> int:
-  app = Application()
-  exit_code: int = app.run()
-  return exit_code
+    app = Application()
+    exit_code: int = app.run()
+    return exit_code
 
 
 if __name__ == "__main__":
-  main()
+    main()
