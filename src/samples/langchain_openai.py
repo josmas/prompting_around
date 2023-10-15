fffc5b8e59ade57e079949d1192266d11f98394b
josmas <josmasflores@gmail.com>
2023-10-15 19:56:21 +0100

    add aimos and an openai + langchain sample file

---
+++ src/samples/langchain_openai.py
@@ -0,0 +1,24 @@
+#%%
+import os
+from langchain.prompts import ChatPromptTemplate
+from langchain.chat_models import ChatOpenAI
+from aimstack.langchain_debugger.callback_handlers import GenericCallbackHandler
+
+#%%
+os.environ["OPENAI_API_KEY"] = ""
+
+#%%
+prompt = ChatPromptTemplate.from_template("tell me a joke about {foo}")
+
+#%%
+aim_cb = GenericCallbackHandler()
+model = ChatOpenAI(callbacks=[aim_cb])
+chain = prompt | model
+
+#%%
+response = chain.invoke({"foo": "beavers"})
+aim_cb.flush()
+
+print(response)
+
+# %%
